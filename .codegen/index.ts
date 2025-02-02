d573e1a8794506409d3c4f7cbbaf5fc4dbe4e109
andrew robertson <andrewcrobertson@gmail.com>
2025-02-02 18:14:41 +1100

    bump-deps

--- .codegen/index.ts
+++ .codegen/index.ts
@@ -1,32 +1,3 @@
-import { nodeAppDockerfile } from '@studio-75/sdk.dockerfile.node-app';
-import { nodeAppDockerGitHubAction } from '@studio-75/sdk.github-action.docker.node-app';
-import { onboard } from '@studio-75/sdk.onboard';
+import { container } from '@valcompare/sdk.container';
 
-const envStandard = {
-  LOG_LEVEL: 'info',
-};
-
-const envDatabase = {
-  PG_DATABASE: '',
-  PG_HOST: '',
-  PG_USER: '',
-  PG_PASSWORD: '',
-  PG_PORT: '',
-};
-
-export const slackConfig = {
-  SLACK_OAUTH_TOKEN: '',
-};
-
-const infisical = { projectSlug: 'execution-environments-v5w-q', secretPath: '/github' };
-const npmSettings = { registry: 'https://npm.pkg.github.com/', scopes: ['studio-75', 'valcompare'] };
-const env = { ...envStandard, ...envDatabase, ...slackConfig };
-const appWebsite = { name: 'Website', package: '@valcompare/app.website', command: 'website', env };
-
-const run = async () => {
-  await onboard();
-  await nodeAppDockerfile({ npmSettings, apps: appWebsite });
-  await nodeAppDockerGitHubAction({ infisical, appNames: appWebsite.name });
-};
-
-run();
+container();
