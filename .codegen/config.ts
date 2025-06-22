59456409d9345a1e107853981eb6ff92249fe927
Andrew Robertson <arobertson@pay.com.au>
2025-06-22 14:52:34 +1000

    add apps

--- .codegen/config.ts
+++ .codegen/config.ts
@@ -20,6 +20,33 @@ const slackConfig = {
 
 const env = { ...envStandard, ...envDatabase, ...slackConfig, ...resendConfig };
 
+export const auth = {
+  name: 'Auth',
+  package: '@valcompare/app.auth',
+  command: 'auth',
+  env,
+  npm: {
+    registry: 'https://npm.pkg.github.com/',
+    readScopes: ['studio-75', 'valcompare'],
+    writeScopes: ['valcompare'],
+  },
+  docker: {
+    imageName: 'ghcr.io/valcompare/container-auth:main',
+    containerName: 'valcompare.auth',
+    envVars: Object.keys(env),
+    hostPort: 3002,
+  },
+  infisical: {
+    gitHubAction: { projectSlug: 'execution-environments-v5w-q', secretPath: '/github', recursive: true },
+    ssh: { projectSlug: 'solutions-z-cbb', secretPath: '/valcompare/ssh' },
+    apps: [
+      { projectSlug: 'solutions-z-cbb', secretPath: '/valcompare/database' },
+      { projectSlug: 'solutions-z-cbb', secretPath: '/valcompare/resend' },
+      { projectSlug: 'solutions-z-cbb', secretPath: '/valcompare/slack' },
+    ],
+  },
+};
+
 export const website = {
   name: 'Website',
   package: '@valcompare/app.website',
@@ -33,6 +60,87 @@ export const website = {
   docker: {
     imageName: 'ghcr.io/valcompare/container-website:main',
     containerName: 'valcompare.website',
+    envVars: Object.keys(env),
+    hostPort: 3002,
+  },
+  infisical: {
+    gitHubAction: { projectSlug: 'execution-environments-v5w-q', secretPath: '/github', recursive: true },
+    ssh: { projectSlug: 'solutions-z-cbb', secretPath: '/valcompare/ssh' },
+    apps: [
+      { projectSlug: 'solutions-z-cbb', secretPath: '/valcompare/database' },
+      { projectSlug: 'solutions-z-cbb', secretPath: '/valcompare/resend' },
+      { projectSlug: 'solutions-z-cbb', secretPath: '/valcompare/slack' },
+    ],
+  },
+};
+
+export const ops = {
+  name: 'Ops',
+  package: '@valcompare/app.ops',
+  command: 'ops',
+  env,
+  npm: {
+    registry: 'https://npm.pkg.github.com/',
+    readScopes: ['studio-75', 'valcompare'],
+    writeScopes: ['valcompare'],
+  },
+  docker: {
+    imageName: 'ghcr.io/valcompare/container-ops:main',
+    containerName: 'valcompare.ops',
+    envVars: Object.keys(env),
+    hostPort: 3002,
+  },
+  infisical: {
+    gitHubAction: { projectSlug: 'execution-environments-v5w-q', secretPath: '/github', recursive: true },
+    ssh: { projectSlug: 'solutions-z-cbb', secretPath: '/valcompare/ssh' },
+    apps: [
+      { projectSlug: 'solutions-z-cbb', secretPath: '/valcompare/database' },
+      { projectSlug: 'solutions-z-cbb', secretPath: '/valcompare/resend' },
+      { projectSlug: 'solutions-z-cbb', secretPath: '/valcompare/slack' },
+    ],
+  },
+};
+
+export const portalAdvisor = {
+  name: 'Advisor Portal',
+  package: '@valcompare/app.portal.advisor',
+  command: 'advisor-portal',
+  env,
+  npm: {
+    registry: 'https://npm.pkg.github.com/',
+    readScopes: ['studio-75', 'valcompare'],
+    writeScopes: ['valcompare'],
+  },
+  docker: {
+    imageName: 'ghcr.io/valcompare/container-ops:main',
+    containerName: 'valcompare.portal-advisor',
+    envVars: Object.keys(env),
+    hostPort: 3002,
+  },
+  infisical: {
+    gitHubAction: { projectSlug: 'execution-environments-v5w-q', secretPath: '/github', recursive: true },
+    ssh: { projectSlug: 'solutions-z-cbb', secretPath: '/valcompare/ssh' },
+    apps: [
+      { projectSlug: 'solutions-z-cbb', secretPath: '/valcompare/database' },
+      { projectSlug: 'solutions-z-cbb', secretPath: '/valcompare/resend' },
+      { projectSlug: 'solutions-z-cbb', secretPath: '/valcompare/slack' },
+    ],
+  },
+};
+
+export const portalCustomer = {
+  name: 'Customer Portal',
+  package: '@valcompare/app.portal.customer',
+  command: 'customer-portal',
+  env,
+  npm: {
+    registry: 'https://npm.pkg.github.com/',
+    readScopes: ['studio-75', 'valcompare'],
+    writeScopes: ['valcompare'],
+  },
+  docker: {
+    imageName: 'ghcr.io/valcompare/container-ops:main',
+    containerName: 'valcompare.portal-customer',
     envVars: Object.keys(env),
     hostPort: 3002,
   },
--- .codegen/index.ts
+++ .codegen/index.ts
@@ -1,15 +1,19 @@
 import { nodeAppDockerfile } from '@studio-75/sdk.dockerfile.node-app';
 import { nodeAppDockerGitHubAction } from '@studio-75/sdk.github-action.docker.node-app';
 import { onboard } from '@studio-75/sdk.onboard';
-import { website } from './config';
+import { auth, ops, portalAdvisor, portalCustomer, website } from './config';
+
+const apps = [auth, website, ops, portalAdvisor, portalCustomer];
 
 export const run = async () => {
   await onboard();
   await nodeAppDockerfile({
     npm: { registry: website.npm.registry, scopes: website.npm.readScopes },
-    apps: { name: website.name, package: website.package, command: website.command, env: website.env },
+    apps: apps.map((app) => ({ name: app.name, package: app.package, command: app.command, env: app.env })),
   });
-  await nodeAppDockerGitHubAction({ infisical: website.infisical.gitHubAction, appNames: website.name });
+  for (let i = 0; i < apps.length; i++) {
+    await nodeAppDockerGitHubAction({ infisical: apps[i].infisical.gitHubAction, appNames: apps[i].name });
+  }
 };
 
 run();
